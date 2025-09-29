4e98524fd5ada572283543dd38a8a7b82cc588d7
lucasmodolo <lucasmodolo@usp.br>
2025-09-29 15:57:08 -0300

    Adicionado algumas funções do arquivo de dados

--- include/DataFile.h
+++ include/DataFile.h
@@ -1,4 +1,3 @@
-
 // DataFile.h
 // Class to abstract the DataFile manipulation
 
@@ -10,12 +9,23 @@ class DataFile {
         DataFile();
         ~DataFile();
 
-        struct registry {
-            int k;
+        struct registry { // celestialBody
+            double mass; // in TeraTons or 10^15 Kg
+            char name[25];
+            float radius; // in Km
+            double distanceFromSun; // in GigaMeters or 10^6 Km
+            float density; // in Kg/m^3
+            float gravityOnSurface; // in m/s^2
+            float rotationPeriod; // in hours
+            float orbitalPeriod; // in days
+            char atmosphereComposition;
+            short satellites;
+            bool isMoon;
         };
 
         registry getNextRegistry();
         registry getNthRegistry(int n);
+        void writeRegistry(registry registry);
         static void printRegistry(registry registry);
         void printFile();
         int getSize();
